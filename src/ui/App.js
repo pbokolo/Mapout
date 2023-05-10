0548b7a59b23e1c10adad5f49b35a904326b5e3c
pbokolo <dev.pbokolo@gmail.com>
2023-05-10 05:12:14 +0200

    Setting up the basics style with scss

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,8 +1,9 @@
 import WorkoutsList from "./components/WorkoutsList";
 import MapView from "./components/MapView";
+import "./style/main.scss";
 function App() {
   return (
-    <div>
+    <div className="app">
       <WorkoutsList />
       <MapView />
     </div>
