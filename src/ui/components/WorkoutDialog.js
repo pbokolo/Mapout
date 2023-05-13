d73af4cbfeaa0243f070adacbd4db787985297fa
pbokolo <dev.pbokolo@gmail.com>
2023-05-13 09:01:05 +0200

    Enhancing the overall style of the dialog

--- src/ui/components/WorkoutDialog.js
+++ src/ui/components/WorkoutDialog.js
@@ -4,7 +4,11 @@ export default function WorkoutDialog() {
   return (
     <div id="dialog" className="dialog__overlay">
       <div className="dialog__content">
-        <h3>A new workout</h3>
+        <div className="dialog__content-header">
+          <h5>Create a new workout</h5>
+        </div>
+        <div className="dialog__content-body">Your form here</div>
+        <div className="dialog__content-footer">Footer</div>
       </div>
     </div>
   );
