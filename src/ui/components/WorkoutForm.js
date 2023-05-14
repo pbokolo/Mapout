b42b5564d6d50c976e118c1f76180995c8bc2319
pbokolo <dev.pbokolo@gmail.com>
2023-05-14 18:40:27 +0200

    Starting the basic style of the form

--- src/ui/components/WorkoutForm.js
+++ src/ui/components/WorkoutForm.js
@@ -2,18 +2,24 @@ import React from "react";
 
 export default function WorkoutForm() {
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <div>
+    <form className="form" onSubmit={(e) => e.preventDefault()}>
+      <fieldset className="fieldset">
+        <label htmlFor="type">Workout</label>
+        <select id="type">
+          <option>Running</option>
+          <option>Cycling</option>
+        </select>
+      </fieldset>
+      <fieldset className="fieldset">
         <label htmlFor="distance">Distance</label>
-        <input type="number" />
-      </div>
-
-      <div>
+        <input type="number" id="distance" />
+      </fieldset>
+      <fieldset className="fieldset">
         <label htmlFor="start">Start</label>
-        <input type="number" />
-        <label htmlFor="start">End</label>
-        <input type="number" />
-      </div>
+        <input type="number" id="start" />
+        <label htmlFor="end">End</label>
+        <input type="number" id="end" />
+      </fieldset>
     </form>
   );
 }
