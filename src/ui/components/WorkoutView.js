d464eacb5ce832baf44230665db0d3b3502545ac
pbokolo <dev.pbokolo@gmail.com>
2023-05-16 10:49:45 +0200

    Adding material icons on workout details

--- src/ui/components/WorkoutView.js
+++ src/ui/components/WorkoutView.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
+import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import TerrainIcon from "@mui/icons-material/Terrain";
+
 export default function WorkoutView({ workout }) {
   const workoutDate = new Date(workout.date);
   const dateStr = new Intl.DateTimeFormat("fr-FR").format(workoutDate);
   const indicator = workout.type === "Running" ? "r" : "c";
+  const icon =
+    workout.type === "Running" ? <DirectionsRunIcon /> : <DirectionsBikeIcon />;
 
   return (
     <div className="workout">
@@ -14,9 +20,17 @@ export default function WorkoutView({ workout }) {
         {" "}
         <p className="workout__title">{`${workout.type} on ${dateStr}`}</p>
         <div className="workout__details">
-          <span className="workout__details-distance">{`${workout.distance} KM`}</span>
+          <span className="workout__details-distance">
+            {" "}
+            {icon} {`${workout.distance} KM`}
+          </span>
           {workout.type === "Cycling" ? (
-            <span className="workout__details-distance">{`${workout.elevation} M`}</span>
+            <span className="workout__details-elevation">
+              <span>
+                <TerrainIcon />
+              </span>
+              {`${workout.elevation} M`}
+            </span>
           ) : (
             ""
           )}
