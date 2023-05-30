61de84cf521b7be182fa04cc72baf80e3ebb39db
pbokolo <dev.pbokolo@gmail.com>
2023-05-30 08:48:50 +0200

    Finnishing up the hamburger menu

--- src/ui/components/Menu.js
+++ src/ui/components/Menu.js
@@ -4,7 +4,7 @@ export default function Menu() {
   return (
     <div className="menu">
       <input type="checkbox" className="menu__chk" id="menu__chk" />
-      <label labelfor="menu__chk" className="menu__chk-lbl">
+      <label htmlFor="menu__chk" className="menu__chk-lbl">
         <span className="menu__icon"></span>
       </label>
       <div className="menu__content"></div>
