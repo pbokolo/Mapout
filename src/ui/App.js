85b55c6e58758375f389869bff6f3a22d5701a44
pbokolo <dev.pbokolo@gmail.com>
2023-05-31 20:55:09 +0200

    Minor refactoring

--- src/ui/App.js
+++ src/ui/App.js
@@ -5,9 +5,9 @@ import { MapView } from "./components";
 import "./style/main.scss";
 import { controller } from "../controller/workout";
 import { controller as mapController } from "../controller/map";
-import { controller as appController } from "../controller/app";
 import { useDispatch } from "react-redux";
 import Menu from "./components/Menu";
+
 function App() {
   const dispatch = useDispatch();
 
@@ -16,25 +16,8 @@ function App() {
     mapController.init(dispatch);
   }, null);
 
-  const handleClick = (e) => {
-    const text = e.target.textContent;
-    if (text === "Go FS") {
-      appController.setFullScreen();
-      e.target.textContent = "Exit FS";
-    } else {
-      appController.exitFullScreen();
-      e.target.textContent = "Go FS";
-    }
-  };
   return (
     <div className="app">
-      {appController.checkUserAgent() ? (
-        <button className="fm-btn" onClick={handleClick}>
-          Go FS
-        </button>
-      ) : (
-        ""
-      )}
       <Menu />
       <MapView />
     </div>
