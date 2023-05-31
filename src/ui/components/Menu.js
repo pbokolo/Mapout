0dcff243930772e9a1603a336df8553d11222a93
pbokolo <dev.pbokolo@gmail.com>
2023-05-31 21:48:53 +0200

    Improving the menu

--- src/ui/components/Menu.js
+++ src/ui/components/Menu.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import WorkoutsList from "./WorkoutsList";
 
 export default function Menu() {
+  const workouts = useSelector((state) => state.workouts.list.length);
   return (
     <div className="menu">
       <input type="checkbox" className="menu__chk" id="menu__chk" />
       <label htmlFor="menu__chk" className="menu__chk-lbl">
         <span className="menu__icon"></span>
+        <span>Workouts({workouts})</span>
       </label>
       <div className="menu__content">
         <WorkoutsList />
